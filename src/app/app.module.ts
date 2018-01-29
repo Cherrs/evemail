9ec4279f9ff58641b92954a4a30b58d45e090d3d
KuriyamaMirai <lovef@outlook.com>
2018-01-29 18:56:28 +0800

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { CallbackComponent } from './callback/callback.component';
+import { EvessoService } from './services/evesso/evesso.service';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CallbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
-  providers: [],
+  providers: [EvessoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
